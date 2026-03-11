a27dd90ff16a78833f6c052d426f1d6b112ffa1f
Omar-NAMOUS <omarnamous.alg@gmail.com>
2026-03-11 17:49:12 +0100

    fix script relative paths

--- perspective/2_finetune/scripts/get_embedding.sh
+++ perspective/2_finetune/scripts/get_embedding.sh
@@ -3,12 +3,12 @@ for dataset in banking77
 do
     for scale in small
     do
-        CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 MKL_NUM_THREADS=4 python get_embedding.py \
+        CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 MKL_NUM_THREADS=4 python ./perspective/2_finetune/get_embedding.py \
             --model_name hkunlp/instructor-large \
             --scale $scale \
             --task_name $dataset \
-            --data_path ../../datasets/${dataset}/${scale}.jsonl \
-            --result_file ../../datasets/${dataset}/${scale}_embeds.hdf5 \
+            --data_path ./datasets/${dataset}/${scale}.jsonl \
+            --result_file ./datasets/${dataset}/${scale}_embeds.hdf5 \
             --measure
     done
 done
